60460f56b91cf5c713df6ab7963e48208af3065c
Giovanni Biancuzzo <39157315+GiovanniBiancuzzo@users.noreply.github.com>
2022-02-01 00:07:41 +0100

    aggiunta della pulizia campi via reset

--- script.ts
+++ script.ts
@@ -9,17 +9,23 @@ $( () => {
         let diff2: number = Math.abs(num2-rand);
 
         if (num1==rand) { //condizioni del gioco
-            $('#input').next().next().next().text('Il giocatore 1 ha azzeccato il numero random');
+            $('#input').next().next().next().text('Il giocatore 1 ha azzeccato il numero random.');
         }
         else if (num2==rand) {
-            $('#input').next().next().next().text('Il giocatore 2 ha azzeccato il numero random');
+            $('#input').next().next().next().text('Il giocatore 2 ha azzeccato il numero random.');
         }
         else if (diff1<diff2) {
-            $('#input').next().next().next().text('Il giocatore 1 si è avvicinato di più al numero random');
+            $('#input').next().next().next().text('Il giocatore 1 si è avvicinato di più al numero random.');
         }
         else if (diff1>diff2) {
-            $('#input').next().next().next().text('Il giocatore 2 si è avvicinato di più al numero random');
+            $('#input').next().next().next().text('Il giocatore 2 si è avvicinato di più al numero random.');
         }
         //probabilmente è poco efficiente usare tre next(), dovuti ai br usati per spaziare
+    })
+
+    $('#reset').on('click', function() {
+        let rand = 0;
+        $('#input').next().text('Il numero random è: ' + rand);
+        $('#input').next().next().next().text('Vediamo quale giocatore si avvicina di più al numero random.');
     })
 });
